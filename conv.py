7e930f38190803325930791727543ea1d5d568c0
Alex Malone <jalexmalone@gmail.com>
2016-02-29 11:14:39 -0600

    properly update movie length

--- conv.py
+++ conv.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2
 
 from sys import argv
+import struct
 
 (StartMask, AMask, BMask, XMask,
     YMask, ZMask, UpMask, DownMask) = (1 << x for x in range(8))
 
 blank = "".join(chr(x) for x in (0, 0, 0, 0, 0x80, 0x80, 0x80, 0x80))
+
+WAIT_FRAMES = 1165 * 2
 
 
 def __main__():
@@ -19,8 +22,11 @@ def __main__():
     with open(outfile, "w") as f:
         data = open(infile, "r").read()
 
-        f.write(data[0:0x100])                   # header
-        f.write(data[0x100:0x100 + 123 * 0x20])  # first 123 frames: "ready, go"
+        VIs, inputs, lag = struct.unpack("<3Q", data[0xD:0x25])
+        data = data[:0xD] + struct.pack("<2Q", (VIs + lag) * 6, inputs * 6) + data[0x1D:]
+
+        f.write(data[0:0x100])                           # header
+        f.write(data[0x100:0x100 + WAIT_FRAMES * 0x20])  # skip to "ready, go"
 
         for input in (StartMask, UpMask, YMask | DownMask, StartMask):
             f.write(chr(input))
@@ -28,7 +34,7 @@ def __main__():
             for port in range(2, 4 + 1):
                 f.write(blank)
 
-        for frame in range(123, (len(data) - 0x100) // 0x20 + 1):
+        for frame in range(WAIT_FRAMES, (len(data) - 0x100) // 0x20 + 1):
             f.write(data[0x100 + 0x20 * frame:0x100 + 0x20 * (frame + 1)])
             for input in (StartMask, XMask | DownMask, XMask | DownMask, XMask | DownMask, XMask | DownMask, StartMask):
                 f.write(chr(input))
